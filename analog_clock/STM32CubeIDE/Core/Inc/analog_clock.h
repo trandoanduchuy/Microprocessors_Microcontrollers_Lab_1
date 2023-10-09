f7d830cf398dec36e6bd2db51d2cbe83c62395fd
huytran <huyducdoantran@gmail.com>
2023-10-09 17:47:08 +0700

    add full ex 6

---
+++ analog_clock/STM32CubeIDE/Core/Inc/analog_clock.h
@@ -0,0 +1,16 @@
+/*
+ * analog_clock.h
+ *
+ *  Created on: Oct 9, 2023
+ *      Author: Huy
+ */
+#include "global.h"
+
+#ifndef INC_ANALOG_CLOCK_H_
+#define INC_ANALOG_CLOCK_H_
+
+void TurnLed(int num);
+
+void TurnLedInSequence();
+
+#endif /* INC_ANALOG_CLOCK_H_ */
---
+++ analog_clock/STM32CubeIDE/Core/Inc/global.h
@@ -0,0 +1,14 @@
+/*
+ * global.h
+ *
+ *  Created on: Oct 9, 2023
+ *      Author: Huy
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "main.h"
+#include "software_timer.h"
+
+#endif /* INC_GLOBAL_H_ */
